f6f102504a5501208e42cd43b9e6ef1bf6df985b
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-08-20 00:45:23 -0700

    beta version of alexa skill

--- index.js
+++ index.js
@@ -27,63 +27,75 @@ AWS.config.update({
   region: AWSregion
 })
 
-const handlers = {
-  'LaunchRequest': function () {
-    let countryCode = ''
-    let postalCode = ''
-    let lat = 0
-    let lng = 0
-    let city = ''
-    let state = ''
-    let timeZoneId = ''
-    const userId = this.event.session.user.userId
-    const consentToken = this.event.session.user.permissions.consentToken
-    const deviceId = this.event.context.System.device.deviceId
+function updateUserLocation(cb) {
+  const userId = this.event.session.user.userId
+  const consentToken = this.event.session.user.permissions.consentToken
+  const deviceId = this.event.context.System.device.deviceId
+  let countryCode = ''
+  let postalCode = ''
+  let lat = 0
+  let lng = 0
+  let city = ''
+  let state = ''
+  let timeZoneId = ''
 
-    var ctx = this
+  axios.get(`https://api.amazonalexa.com/v1/devices/${deviceId}/settings/address/countryAndPostalCode`, {
+    headers: { 'Authorization': `Bearer ${consentToken}` }
+  })
+  .then((response) => {
+    countryCode = response.data.countryCode
+    postalCode = response.data.postalCode
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${countryCode},${postalCode}&key=${process.env.GOOGLE_MAPS_KEY}`)
+  })
+  .then((response) => {
+    city = response.data.results[0].address_components[1].short_name
+    state = response.data.results[0].address_components[3].short_name
+    lat = response.data.results[0].geometry.location.lat
+    lng = response.data.results[0].geometry.location.lng
+    return axios.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${moment().unix()}&key=${process.env.GOOGLE_MAPS_KEY}`)
+  })
+  .then((response) => {
+    timeZoneId = response.data.timeZoneId
+    getParams.Key.userId = userId
 
-    axios.get(`https://api.amazonalexa.com/v1/devices/${deviceId}/settings/address/countryAndPostalCode`, {
-      headers: { 'Authorization': `Bearer ${consentToken}` }
-    })
-    .then((response) => {
-      countryCode = response.data.countryCode
-      postalCode = response.data.postalCode
-      return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${countryCode},${postalCode}&key=${process.env.GOOGLE_MAPS_KEY}`)
-    })
-    .then((response) => {
-      city = response.data.results[0].address_components[1].short_name
-      state = response.data.results[0].address_components[3].short_name
-      lat = response.data.results[0].geometry.location.lat
-      lng = response.data.results[0].geometry.location.lng
-      return axios.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${moment().unix()}&key=${process.env.GOOGLE_MAPS_KEY}`)
-    })
-    .then((response) => {
-      timeZoneId = response.data.timeZoneId
-      getParams.Key.userId = userId
+    // get current user data
+    readDynamoItem(getParams, user => {
+      putParams.Item.userId = userId
+      if (user.milks)
+        putParams.Item.milks = user.milks
+      if (user.unit)
+        putParams.Item.unit = user.unit
+      putParams.Item.countryCode = countryCode
+      putParams.Item.postalCode = postalCode
+      putParams.Item.city = city
+      putParams.Item.state = state
+      putParams.Item.timeZoneId = timeZoneId
+      putParams.Item.lat = lat
+      putParams.Item.lng = lng
 
-      // get current user data
-      readDynamoItem(getParams, user => {
-        putParams.Item.userId = userId
-        if (user.milks)
-          putParams.Item.milks = user.milks
-        if (user.unit)
-          putParams.Item.unit = user.unit
-        putParams.Item.countryCode = countryCode
-        putParams.Item.postalCode = postalCode
-        putParams.Item.city = city
-        putParams.Item.state = state
-        putParams.Item.timeZoneId = timeZoneId
-        putParams.Item.lat = lat
-        putParams.Item.lng = lng
-
-        // update user data
-        putUser(putParams, result => {
-          ctx.emit(':tell', `Launch Request! ${timeZoneId}`)
-        })
+      // update user data
+      putUser(putParams, result => {
+        cb(false)
       })
     })
-    .catch((err) => {
-      ctx.emit(':tell', `Error with Milky Baby!`)
+  })
+  .catch((err) => {
+    cb(true)
+  })
+}
+
+const handlers = {
+  'LaunchRequest': function () {
+    const ctx = this
+    updateUserLocation.call(this, (err) => {
+      if (err) {
+        ctx.emit(':tell', `Error with Milky Baby!`)
+      } else {
+        ctx.emit(':tell', `Welcome to the Milky Baby Skill, add your feeding baby milk amount to your account by saying for example: 
+          'ask milky baby to add 60 oz'. This will save this data to your account and we we will provide you
+          summarized information by saying: 'ask milky baby for a status'.
+          Thanks for using Milky Baby.`)
+      }
     })
   },
 
@@ -91,7 +103,11 @@ const handlers = {
     const userId = this.event.session.user.userId
     getParams.Key.userId = userId
 
-    const { amount, unit } = this.event.request.intent.slots
+    const { amount: amountStr, unit } = this.event.request.intent.slots
+    const amount = parseInt(amountStr.value, 10)
+    if (isNaN(amount))
+      this.emit(':tell', "Please indicate a correct number to add, for example: 'ask milky baby to add 60 oz'")
+
     const currDate = new moment()
 
     readDynamoItem(getParams, user => {
@@ -100,9 +116,9 @@ const handlers = {
         milks = user.milks
       }
 
-      const date = currDate.tz(user.timeZoneId).format('YYYY-M-D h:mm:ss a')
+      const date = currDate.tz(user.timeZoneId).format('YYYY-MM-DD HH:mm')
 
-      milks.push({ amount: amount.value, unit: unit.value, date })
+      milks.push({ amount, unit: unit.value, date })
       putParams.Item.milks = milks
       putParams.Item.userId = userId
       putParams.Item.unit = unit.value
@@ -122,7 +138,7 @@ const handlers = {
         putParams.Item.lng = user.lng
 
       putUser(putParams, result => {
-        this.emit(':tell', `You selected ${amount.value} ${unit.value}`)
+        this.emit(':tell', `${amount} ${unit.value} added.`)
       })
     })
   },
@@ -132,12 +148,23 @@ const handlers = {
     getParams.Key.userId = userId
 
     readDynamoItem(getParams, user => {
+      const date = new moment()
+      const today = date.tz(user.timeZoneId)
       const unit = user.unit
+      let lastFeefingTime = ''
       let total = 0
-      user.milks.forEach(m => {
-        total += parseInt(m.amount, 10)
+
+      user.milks.forEach((m, key) => {
+        const dateItem = moment(m.date)
+        if (today.day() === dateItem.day()) {
+          total += m.amount
+        }
+
+        if (user.milks.length - 1 === key) {
+          lastFeefingTime = dateItem.format('h:mm A')
+        }
       })
-      this.emit(':tell', `Your baby consumed about ${total} ${unit} today. The next feeding time is at 4pm`)
+      this.emit(':tell', `Your baby consumed about ${total} ${unit} today. The last feeding time was at ${lastFeefingTime}.`)
     })
   },
 
